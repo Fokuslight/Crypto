ebc1f3c379db721a1c713f3c2b93c28b030e891d
Aziz <shift_s@mail.ru>
2020-04-06 13:11:46 +0300

    Add file upload
    Add file upload

---
+++ cryptoapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.cryptoapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.cryptoapp.api.ApiFactory
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appViewModel: AppViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        appViewModel = ViewModelProviders.of(this)[AppViewModel::class.java]
+
+//        appViewModel.priceList.observe(this, Observer {
+//            Log.d("Success", "in activity $it")
+//        })
+
+        appViewModel.getDetailInfo("ETC").observe(this, Observer {
+            Log.d("Success2", "in activity $it")
+        })
+
+    }
+
+
+}
